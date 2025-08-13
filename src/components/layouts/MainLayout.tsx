bd4ca49284f90bbbac38c6583dbfba5dc50decc8
yuzukq <yuzupanda1006@icloud.com>
2025-08-13 21:41:19 +0900

    コンポーネントの配置を検証，本番環境からの退避

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import TopSection from "../sections/TopSection";
+
+export default function MainLayout() {
+  return (
+    <div>
+      <Header />
+      <TopSection />
+      <Footer />
+    </div>
+  )
+}
