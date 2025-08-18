f70f4f0bde6d9948bc08ecaae9ae415d2b9c5b0a
yuzukq <yuzupanda1006@icloud.com>
2025-08-18 22:12:00 +0900

    レイアウトページを作成

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 import Header from "./Header/Header";
 import Footer from "./Footer";
-import TopSection from "../sections/TopSection";
+import TopSection from "@/components/sections/TopSection";
+import AboutSection from "@/components/sections/AboutSection";
 
 export default function MainLayout() {
   return (
     <div>
       <Header />
       <TopSection />
+      <AboutSection />
       <Footer />
     </div>
   )
