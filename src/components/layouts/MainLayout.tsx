fa372c46b75093473c92d61d05dea954ad131729
VR-Yuzu <109357821+yuzukq@users.noreply.github.com>
2025-08-20 22:54:41 +0900

    storypageのひな型を作成

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 import Header from "./Header/Header";
 import Footer from "./Footer";
 import TopSection from "../sections/TopSection";
+import StorySection from "../sections/StorySection";
 
 export default function MainLayout() {
   return (
     <div>
       <Header />
       <TopSection />
+      <StorySection />
       <Footer />
     </div>
   )
