cfdec9b334f4772c1164e936b9de89bdd4b32393
VR-Yuzu <109357821+yuzukq@users.noreply.github.com>
2025-10-03 00:04:13 +0900

    Storyセクション見出し部分のスタイリングを微調整

--- src/components/sections/StorySection.tsx
+++ src/components/sections/StorySection.tsx
@@ -5,7 +5,7 @@ import StoryTimeline from "@/components/ui/storyTimeline";
 export default function StorySection() {
   return (
     <SectionContainer backgroundColor="light" id="story">
-      <Flex direction="column" align="center" justify="center" h={20}>
+      <Flex direction="column" align="center" justify="center" py={10}>
         <Heading size="2xl" color="black">My Story</Heading>
           <Text color="gray.800" mb={10} textAlign="center" maxW="600px">
             現在に至るまでの振り返り。
