6a03e7b7edc2f478aad7b156e18b428beb6e7255
yuzukq <yuzupanda1006@icloud.com>
2025-10-02 21:59:46 +0900

    引き継ぎ

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -9,33 +9,61 @@ export interface Story {
 
 export const stories: Story[] = [
   {
-    id: "dmyan1",
-    date: "2020年10月",
-    title: "テストタイトル",
-    description: "テスト説明",
+    id: "enrollment",
+    date: "2023年4月",
+    title: "情報工学科(CS)に入学",
+    description: "ソフトとハードの両面からコンピュータについて学べるということ．これまで利用してきたアプリケーションやコンピュータの基礎となる技術を構築する技術を学べるという点に魅力を感じ，現在の大学に入学しました．",
   },
   {
-    id: "dammy2",
-    date: "長い長い長い長い日付",
-    title: "長い長い長い長い長い長いタイトル",
-    description: "長い長い長い長い長い長い長い説明",
+    id: "joined_the_cycling_club",
+    date: "2023年4月",
+    title: "サイクリング部に入部",
+    description: "中学時代から好きだったロードバイクを大学でも続けるためにサイクリング部に入部．学業や開発の息抜きとして，月例ランや合宿など，仲間たちと自転車を楽しんでいます．",
   },
   {
-    id: "dammy3",
-    date: "長い長い長い長い長い長い長い長い長い長い長い日付",
-    title: "長い長い長い長い長い長い長い長い長い長い長いタイトル",
-    description: "長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い長い説明",
+    id: "start_vr",
+    date: "2023年10月",
+    title: "VRとの出会い",
+    description: "Meta Quest3の発売に合わせてVRの世界に脚を踏み込む．数か月後には自宅にライトハウスを構築するまでに...Unity上での簡単な改変をきっかけに，後のワールド制作やBSモデリングを始めることになる．",
   },
   {
-    id: "dammy4",
-    date: "ダミー日付",
-    title: "ダミータイトル",
-    description: "ダミー説明",
+    id: "joined_the_softmedia_club",
+    date: "2024年4月",
+    title: "ソフトメディア研究会に入部",
+    description: `ともにVRを始めた学科の友人の影響で．技術系サークルに入部．P(プログラム),D(DTM),M(マルチメディア)の三班にわかれ，主にゲーム制作を行っています．
+    ProductセクションにあるようなVR上での実装や，4DCと呼ばれる4日間でゲームを開発する行事に参加しています．`,
   },
   {
-    id: "dammy5",
-    date: "ダミー日付",
-    title: "ダミータイトル",
-    description: "ダミー説明",
-  }
+    id: "unix",
+    date: "2024年6月",
+    title: "Unixとの出会い",
+    description: `大学のサーバ管理の実験で様々なLinuxディストリビューション,BSDに触れることに．BSD御三家の歴史的開発背景を知り，OSSに興味を持つ．
+    VM上でNginxやApacheなどのWebサーバを導入して，限られたリソースの中で最大のRPSを目指すという課題に対してFreeBSDを選定．コミュニティで議論されていたカーネルパラメータを参考に，人生初のカーネル再コンパイル．
+    環境構築を再現させる仕様書作成のタスクでは，個人的にRed Hat系，Debian系，BSD系の様々なOSを導入しました．
+    現在はこれらの経験からArchとWinの二台メインで運用．それぞれHyprlandやGlazeWMといったOSSを導入して飼っています．`,
+  },
+  {
+    id: "entry_web",
+    date: "2024年9月",
+    title: "Rubyとの出会い",
+    description: `学部二年の後期に「プログラミング言語」という授業がありました．`,
+  },
+  {
+    id: "hack1",
+    date: "2025年5月",
+    title: "はじめてのハッカソン",
+    description: ``,
+  },
+  {
+    id: "sky",
+    date: "2025年8月",
+    title: "Sky株式会社でのフロントエンド長期インターン",
+    description: ``,
+  },
+  {
+    id: "gmo_pb",
+    date: "2024年9月",
+    title: "GMOペパボ-SUZURI事業部での長期開発インターン",
+    description: ``,
+  },
 ];
